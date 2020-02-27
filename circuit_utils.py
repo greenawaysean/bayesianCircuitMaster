26e65913a28d8a3bf5b7b2b2ee21c7a269423748
Sean G <greenawaysean@yahoo.co.uk>
2020-02-27 14:20:01 +0000

    Changed formalism to QuantumInstance

--- circuit_utils.py
+++ circuit_utils.py
@@ -1,7 +1,11 @@
-from typing import List
+from typing import List, Union
 import numpy as np
+import copy
 import itertools
 from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit, execute
+from qiskit.aqua import QuantumInstance
+from qiskit.circuit import Parameter
+from qiskit.compiler import transpile
 from probability_distributions import ProbDist
 from general_utils import GateObj
 
@@ -17,99 +21,170 @@ class qCirc:
     nqubits: number of qubits
     input_state: initial state settings for the simulation
     V: list of GateObj's defining the simulated unitary evolution
+    params_list: list of parameters to populate the circuit with
+
     meas_basis: settings for the measurement basis
+
+    Methods:
+    --------
+    apply_init_state: input state
+    apply_V: (Trotterised) unitary evolution operator, parameterised for optimisation
+    rotate_meas_basis: rotate into the eigenbasis of the desired observable
+    build_circuit: combine the three independent circuits together
     """
 
     def __init__(self, nqubits: int, input_state: List[GateObj], V: List[GateObj],
-                 meas_basis: List[GateObj]):
+                 meas_basis: List[GateObj], backend: str, init_layout: dict = None):
         self.nqubits = nqubits
         self.input_state = input_state
         self.V = V
         self.meas_basis = meas_basis
-
-        self.qreg = QuantumRegister(self.nqubits)
+        self.backend = backend
+        self.init_layout = init_layout
+        self.qreg = QuantumRegister(self.nqubits, name='qreg')
         self.creg = ClassicalRegister(self.nqubits)
         self.qc = QuantumCircuit(self.qreg, self.creg)
+        self.build_circuit()
+        self.qc = transpile(self.qc, backend=self.backend, initial_layout=self.init_layout)
+
+    def populate_circuits(self, params_list):
+        circ = copy.deepcopy(self.qc)
+        param_dict = self.generate_params_dict(params_list)
+        circ = circ.bind_parameters(param_dict)
+
+        return circ
+
+    def generate_params_dict(self, params_list):
+        params_dict = {}
+        idx = 0
+        for _gate in self.V:
+            if _gate.parameterise:
+                params_dict[self.params[idx]] = params_list[idx]
+                idx += 1
+        return params_dict
 
     def apply_init_state(self):
         for _gate in self.input_state:
             apply_gate(self.qc, self.qreg, _gate)
 
     def apply_V(self):
+        params = []
+        idx = 0
         for _gate in self.V:
-            apply_gate(self.qc, self.qreg, _gate)
+            if _gate.parameterise:
+                if _gate.name == 'U3':
+                    params.append((Parameter(f'{idx}'),
+                                   Parameter(f'{idx + 1}'),
+                                   Parameter(f'{idx + 2}')))
+                    apply_gate(self.qc, self.qreg, _gate, parameterise=True,
+                               param=params[-1])
+                    idx += 3
+                else:
+                    params.append(Parameter(f'{idx}'))
+                    apply_gate(self.qc, self.qreg, _gate, parameterise=True,
+                               param=params[-1])
+                    idx += 1
+            else:
+                apply_gate(self.qc, self.qreg, _gate)
+        self.params = tuple(params)
 
     def rotate_meas_basis(self):
+        meas_idx = []
         for _gate in self.meas_basis:
             apply_gate(self.qc, self.qreg, _gate)
+            meas_idx.append(_gate.qubits)
             self.qc.measure(self.qreg[_gate.qubits], self.creg[_gate.qubits])
 
     def build_circuit(self):
+        """Builds seperate circuits for input states, observables and unitary
+        evolution, with the first two being static and the latter being parameterised.
+
+        The full circuit is built by composing all three circuits together.
+        """
         self.apply_init_state()
         self.apply_V()
         self.rotate_meas_basis()
 
-        return self.qc
-
 
 class EstimateCircuits:
-    """Generate a list of circuits which when run estimate the fidelity for a given
-    unitary operator, given a precalculated probability distribution
+    """Class to handle generating circuits for fidelity estimation.
 
-    Attributes:
-    -----------
-    prob_dist: probability distribution object
-    prob_dict: probability distribution from which the measurement settings
-    should be chosen
-    V: ideal unitary operator
-    nqubits: number of qubits
-    length: number of measurement settings to select from
-    settings: measurement settings in the IBMQ format (back-to-front qubits)
-    qutip_settings: settings in the more usual format (reversed wrt qiskit)
+    Builds all possible circuits as parameterised qiskit circuits, selects subsets of
+    them for fidelity estimation and runs them with a given set of parameters.
     """
 
     def __init__(self, prob_dist: ProbDist, V: List[GateObj], nqubits: int,
-                 length: int):
+                 length: int, num_shots: int, backend: str, init_layout: dict):
         self.prob_dist = prob_dist
         self.prob_dict = self.prob_dist.probabilities
+        self.chi_dict = self.prob_dist.chi_dict
         self.V = V
         self.nqubits = nqubits
         self.length = length
-        self.settings = None
-        self.qutip_settings = None
+        self.num_shots = num_shots
+        self.backend = backend
+        self.init_layout = init_layout
+        self.circuits = self.generate_circuits()
+        self.quant_inst = QuantumInstance(backend=self.backend, shots=self.num_shots,
+                                          initial_layout=self.init_layout,
+                                          skip_qobj_validation=False)
 
-    def calculate_fidelity(self, num_shots: int, backend):
-        """Calculate the fidelity using the method in https://arxiv.org/abs/1104.4695
-
-        TODO: generalise this for different approaches to fidelity eastimation
-        """
-        expects = self.run_circuits(num_shots, backend)
-        chi_dict = self.prob_dist.chi_dict
-        ideal_chi = [chi_dict[i] for i in self.qutip_settings]
-
+    def calculate_fidelity(self, params):
+        probs = [self.prob_dict[key] for key in self.prob_dict]
+        keys = [key for key in self.prob_dict]
+        settings, qutip_settings = self.select_settings(probs, keys)
+        ideal_chi = [self.chi_dict[i] for i in qutip_settings]
+        expects = self.run_circuits(settings, params)
         fidelity = 0
         for i, _chi in enumerate(ideal_chi):
-            fidelity += expects[i]/_chi
+            fidelity += expects[i] / _chi
         fidelity /= self.length
 
         return np.abs(fidelity)
 
     def generate_circuits(self):
-        """Generate a list of circuits which can be used to estimate the fidelity"""
-        probs = [self.prob_dict[key] for key in self.prob_dict]
-        keys = [key for key in self.prob_dict]
-        self.settings, self.qutip_settings = self.select_settings(probs, keys)
-        circs = []
-        for _setting in self.settings:
-            init_state, observ = self.parse_setting(_setting)
-            _circ = qCirc(self.nqubits, init_state, self.V, observ).build_circuit()
-            circs.append(_circ)
+        """Builds circuits for all possible combinations of input states and
+        observables.
 
+        Returns:
+        --------
+        circs: dictionary indexing all possible circuits for fidelity estimation
+        """
+        settings = [key for key in self.prob_dict]
+        circs = {}
+        for _setting in settings:
+            init_state, observ = self.parse_setting(_setting)
+            _circ = qCirc(self.nqubits, init_state,
+                          self.V, observ, self.backend, self.init_layout)
+            circs[_setting] = _circ
         return circs
+
+    def run_circuits(self, settings, params):
+        """Choose a subset of <length> circuits for fidelity estimation and run them
+
+        Parameters:
+        -----------
+        params: list of parameters to populate the circuits with (intended to be
+        adapted through optimisation)
+
+        Returns:
+        --------
+        expects: list of expectation values for each circuit in the list
+        """
+        chosen_circs = [self.circuits[_setting] for _setting in settings]
+        exec_circs = []
+        exec_circs = [qc.populate_circuits(params) for qc in chosen_circs]
+        results = self.quant_inst.execute(exec_circs, had_transpiled=True)
+        expects = [
+            generate_expectation(results.get_counts(i)) for i in range(len(exec_circs))
+        ]
+
+        return expects
 
     def select_settings(self, probs, keys):
         """Choose a set of settings given a probability distribution"""
-        choices = np.random.choice([i for i in range(len(keys))], self.length, p=probs)
+        choices = np.random.choice(
+            [i for i in range(len(keys))], self.length, p=probs)
         qutip_settings = [keys[i] for i in choices]
         # qutip and qiskit use mirrored qubit naming schemes
         settings = []
@@ -118,8 +193,9 @@ class EstimateCircuits:
             setting1 = _set[1][::-1]
             settings.append((setting0, setting1))
 
-        settings = [item for item in settings if item[1] != '0'*self.nqubits]
-        qutip_settings = [item for item in qutip_settings if item[1] != '0'*self.nqubits]
+        settings = [item for item in settings if item[1] != '0' * self.nqubits]
+        qutip_settings = [
+            item for item in qutip_settings if item[1] != '0' * self.nqubits]
         return settings, qutip_settings
 
     def parse_setting(self, setting):
@@ -130,11 +206,14 @@ class EstimateCircuits:
             if _op == '0':
                 continue
             elif _op == '1':
-                _s = GateObj(name='X', qubits=i, parameterise=False, params=None)
+                _s = GateObj(name='X', qubits=i,
+                             parameterise=False, params=None)
             elif _op == '2':
-                _s = GateObj(name='Y', qubits=i, parameterise=False, params=None)
+                _s = GateObj(name='Y', qubits=i,
+                             parameterise=False, params=None)
             elif _op == '3':
-                _s = GateObj(name='Z', qubits=i, parameterise=False, params=None)
+                _s = GateObj(name='Z', qubits=i,
+                             parameterise=False, params=None)
             init_state.append(_s)
         observe = []
         for i, _op in enumerate(_obs):
@@ -142,35 +221,27 @@ class EstimateCircuits:
             if _op == '0':
                 continue
             elif _op == '1':
-                _o = GateObj(name='H', qubits=i, parameterise=False, params=None)
+                _o = GateObj(name='H', qubits=i,
+                             parameterise=False, params=None)
             elif _op == '2':
-                _o = GateObj(name='HSdag', qubits=i, parameterise=False, params=None)
+                _o = GateObj(name='HSdag', qubits=i,
+                             parameterise=False, params=None)
             elif _op == '3':
-                _o = GateObj(name='I', qubits=i, parameterise=False, params=None)
+                _o = GateObj(name='I', qubits=i,
+                             parameterise=False, params=None)
             observe.append(_o)
 
         return init_state, observe
 
-    def run_circuits(self, num_shots: int, backend):
-        """Run circuits on a real (IBMQ) or simulated (Aer) qiskit backend
 
-        NOTE: Have seen transpile used in other code, but doesn't seem to work here,
-        need to investigate why/if it is needed
-        """
-        circs = self.generate_circuits()
-        # qjobs = [transpile(c, optimization_level=2) for c in circs]
-        results = execute(circs, backend=backend, shots=num_shots).result()
-        expects = [generate_expectation(results.get_counts(i))
-                   for i in range(len(circs))]
-
-        return expects
-
-
-def apply_gate(circ: QuantumCircuit, qreg: QuantumRegister, gate: GateObj):
+def apply_gate(circ: QuantumCircuit, qreg: QuantumRegister, gate: GateObj,
+               parameterise: bool = False, param: Union[Parameter, tuple] = None):
     """Applies a gate to a quantum circuit.
 
     More complicated gates such as RXX gates should be decomposed into single qubit
-    gates and CNOTs prior to calling this function.
+    gates and CNOTs prior to calling this function. If parameterise is True, then
+    qiskit's placeholder parameter theta will be used in place of any explicit
+    parameters.
     """
     if not isinstance(gate.qubits, list):
         q = gate.qubits
@@ -190,12 +261,23 @@ def apply_gate(circ: QuantumCircuit, qreg: QuantumRegister, gate: GateObj):
         elif gate.name == 'Z':
             circ.z(qreg[q])
         elif gate.name == 'RX':
-            circ.rx(params, qreg[q])
+            if parameterise:
+                circ.rx(param, qreg[q])
+            else:
+                circ.rx(params, qreg[q])
         elif gate.name == 'RY':
-            circ.ry(params, qreg[q])
+            if parameterise:
+                circ.ry(param, qreg[q])
+            else:
+                circ.ry(params, qreg[q])
         elif gate.name == 'RZ':
-            circ.rz(params, qreg[q])
+            if parameterise:
+                circ.rz(param, qreg[q])
+            else:
+                circ.rz(params, qreg[q])
         elif gate.name == 'U3':
+            if parameterise:
+                circ.u3([i for i in param], qreg[q])
             circ.u3(params[0], params[1], params[2], qreg[q])
     else:
         cntrl = gate.qubits[0]
