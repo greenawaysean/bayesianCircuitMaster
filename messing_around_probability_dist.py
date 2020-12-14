93bafbc1eaa17490a8785220e6bab25c46ffae21
Sean Greenaway <sean.greenaway@imperial.ac.uk>
2020-12-14 18:06:25 +0000

    Getting rid of testing files

--- messing_around_probability_dist.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-probs = [1.0] + [0.0]*9
-
-choices = np.random.choice([i for i in range(10)], 500, p=probs, replace=True)
-
-print(choices)
--- noise_model_testing.py
+++
@@ -1,2 +0,0 @@
-import numpy as np
-from qutip import cnot, sigmax, sigmay, sigmaz
